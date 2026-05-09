f9b14c4c6dafad57fcf3216f612930be37fadd3e
igigamemaker <x4w86f72qs@privaterelay.appleid.com>
2026-05-09 13:06:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    minWidth: 960,
+    minHeight: 540,
+    backgroundColor: '#21152f',
+    autoHideMenuBar: true,
+    show: false,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: false,
+      webSecurity: true
+    }
+  });
+
+  Menu.setApplicationMenu(null);
+
+  win.once('ready-to-show', () => {
+    win.show();
+    win.focus();
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
